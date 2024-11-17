35a4ffa7dc83bceca0d2aad1f7d67545d73ec7e7
AndDev <23031429@itcelaya.edu.mx>
2024-11-17 15:59:06 -0600

    netlify addapter added

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,17 +4,16 @@ import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
 
-import cloudflare from '@astrojs/cloudflare';
-
 import db from '@astrojs/db';
 
 import vue from '@astrojs/vue';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-
   integrations: [mdx(), sitemap(), db(), vue()],
   output: 'hybrid',
-  adapter: cloudflare(),
-}); 
+  adapter: netlify(),
+});
